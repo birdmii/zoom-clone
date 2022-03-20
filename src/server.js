48ab3adbffc4808453c42aa7ddd53199ea01aea1
jung.sammie <jung.sammie@gmail.com>
2022-03-20 21:06:32 +0900

    #0.4 Recap & Fix Server.js code

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-import path from "path";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = 3000;
-const __dirname = path.resolve();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 app.listen(PORT, handleListen);
