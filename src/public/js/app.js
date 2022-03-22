fe0dfa0a996f731eb88db57d43389cddba2b0b4c
jung.sammie <jung.sammie@gmail.com>
2022-03-22 20:10:08 +0900

    #2.4 Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,24 @@
 const socket = io();
 
 const welcome = document.querySelector("#welcome");
+const room = document.getElementById("room");
 const form = welcome.querySelector("form");
+
+room.hidden = true;
+let roomNm;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Chat Room Name🍕 ${roomNm}`;
+}
 
 function handleRoomSubmit(e) {
   e.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("CB funtion")
-  });
+  socket.emit("enter_room", input.value, showRoom);
+  roomNm = input.value;
   input.value = "";
 }
 
--- src/server.js
+++ src/server.js
@@ -23,11 +23,10 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    }, 10000);
+  socket.onAny((event) => console.log(`Socket Event: ${event}`));
+  socket.on("enter_room", (roomNm, done) => {
+    socket.join(roomNm);
+    done();
   });
 });
 
