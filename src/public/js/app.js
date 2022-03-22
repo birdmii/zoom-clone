8fd0e08cdeccec9970b89d684e2dba8270c58128
jung.sammie <jung.sammie@gmail.com>
2022-03-22 14:06:02 +0900

    #1.7 Nickname part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,21 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nickname");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server 👍");
 });
 
 socket.addEventListener("message", (msg) => {
-  console.log("New message:", msg.data);
+  const li = document.createElement("li");
+  li.innerText = msg.data
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -18,8 +26,15 @@ function handleSubmit(e) {
   e.preventDefault();
 
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("message", input.value));
   input.value = "";
 }
 
+function handleNicknameSubmit(e) {
+  e.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit)
+nicknameForm.addEventListener("submit", handleNicknameSubmit)
